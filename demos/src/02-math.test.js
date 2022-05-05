f135050b0287d5e5d45ae757cee8e2c5fa7ec770
emproy91 <emproy91@gmail.com>
2022-05-04 21:50:35 -0500

    C11 se implementaron las utilidades en la clase de pruebas Person y teoría de tipos de pruebas

--- demos/src/02-math.test.js
+++ demos/src/02-math.test.js
@@ -2,35 +2,48 @@ const {
   sum, multiply, divide, cylinderVol,
 } = require('./02-math');
 
-test('adds 1 + 3 should be 4', () => { // escenario de bruebas.
-  const rta = sum(1, 3); // resolver la prueba.
-  expect(rta).toBe(4); // resolver la hipotesis.
-});
+// eslint-disable-next-line max-len
+// Agrupar los casos nos deja una mejor organización en el documento del test y en la salida de terminal.
 
-test('should be 4', () => {
-  const rta = multiply(1, 4);
-  expect(rta).toBe(4);
-});
+describe('Test for math', () => {
+  describe('test for sum', () => {
+    test('adds 1 + 3 should be 4', () => {
+      // escenario de bruebas.
+      const rta = sum(1, 3); // resolver la prueba.
+      expect(rta).toBe(4); // resolver la hipotesis.
+    });
+  });
 
-test('should divide', () => {
-  const rta = divide(6, 3);
-  expect(rta).toBe(2);
+  describe('test for multiply', () => {
+    test('should be 4', () => {
+      const rta = multiply(1, 4);
+      expect(rta).toBe(4);
+    });
+  });
 
-  const rta2 = divide(7, 2);
-  expect(rta2).toBe(3.5);
-});
+  describe('test for divide', () => {
+    test('should divide', () => {
+      const rta = divide(6, 3);
+      expect(rta).toBe(2);
 
-test('should divide for zero', () => {
-  const rta = divide(6, 0);
-  expect(rta).toBeNull();
+      const rta2 = divide(7, 2);
+      expect(rta2).toBe(3.5);
+    });
 
-  const rta2 = divide(7, 0);
-  expect(rta2).toBeNull();
-});
+    test('should divide for zero', () => {
+      const rta = divide(6, 0);
+      expect(rta).toBeNull();
 
-test('should be 38', () => {
-  const rta = cylinderVol(2, 3);
-  expect(rta).toBe(38);
+      const rta2 = divide(7, 0);
+      expect(rta2).toBeNull();
+    });
+  });
+  describe('test for volume', () => {
+    test('should be 38', () => {
+      const rta = cylinderVol(2, 3);
+      expect(rta).toBe(38);
+    });
+  });
 });
 
 /* OUT
