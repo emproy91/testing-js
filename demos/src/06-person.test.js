7b046dfbe64b5320f6be0bfa066bfbcf40800f1b
emproy91 <emproy91@gmail.com>
2022-05-07 17:33:14 -0500

    c15 La mayoria ha sido teoria, -- --coverage, index de la carpeta larga de coverage para ver lineas sin testear.

--- demos/src/06-person.test.js
+++ demos/src/06-person.test.js
@@ -4,12 +4,16 @@ const Person = require('./06-person');
 
 describe('Test for Person', () => { // Se agrupan las pruebas de rango IMC
   let person; // Se instancia el objeto persona para automatizar.
+  // Arrange
   beforeEach(() => {
     person = new Person('Rigo', 45, 1.7); // Asignamos datos a la nueva persona.
   });
   test('should return down', () => {
+    // Arrange
     person.weight = 45; // Cambiamos los datos de la persona para este rango/caso.
+    // Act
     const imc = person.calcIMC();
+    // Assert
     expect(imc).toBe('down');
   });
 
